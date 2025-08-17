46a14e3fb87a0b2f9f052ee691e8bc038d83d1fe
behzadhaki <behzadhaki88@gmail.com>
2025-08-17 23:39:24 +0200

    preliminary analysis for second config with smaller tokens trained via flex model

--- helpers/control_feature_utils.py
+++ helpers/control_feature_utils.py
@@ -316,3 +316,213 @@ def run_inference_and_extract_features(model, dataloader):
         ))
 
     return gt_data, pred_data, metadata
+
+
+def run_inference_and_extract_features_flex_model(model, dataloader):
+    model.eval()
+
+    gt_data = {
+        'input_grooves': [],
+        'output_streams': [],
+        'encoding_control1_tokens': [],
+        'encoding_control2_tokens': [],
+        'decoding_control1_tokens': [],
+        'decoding_control2_tokens': [],
+        'decoding_control3_tokens': []
+    }
+
+    pred_data = {
+        'output_streams': [],
+        'flat_outputs': [],
+        'encoding_control1_tokens': [],  # hamming hit of groove to flat output
+        'encoding_control2_tokens': [],  # Accent hits of groove to Accent hits of flat output
+        'decoding_control1_tokens': [],
+        'decoding_control2_tokens': [],
+        'decoding_control3_tokens': []
+    }
+
+    metadata = []
+
+    with torch.no_grad():
+        for i, batch in tqdm.tqdm(enumerate(dataloader), total=len(dataloader)):
+            input_grooves_batch = batch[0]
+            output_streams_gt_batch = batch[1]
+            encoding_control_tokens = batch[2]
+            decoding_control_tokens = batch[3]
+            encoding_control1_tokens_batch = encoding_control_tokens[:, 0]
+            encoding_control2_tokens_batch = encoding_control_tokens[:, 1]
+            decoding_control1_tokens_batch = decoding_control_tokens[:, 0]
+            decoding_control2_tokens_batch = decoding_control_tokens[:, 1]
+            decoding_control3_tokens_batch = decoding_control_tokens[:, 2]
+            indices_batch = batch[-1]
+            metadata.extend([dataloader.dataset.metadata[i] for i in indices_batch])
+
+            # Store ground truth data
+            gt_data['input_grooves'].append(input_grooves_batch)
+            gt_data['output_streams'].append(output_streams_gt_batch)
+            gt_data['encoding_control1_tokens'].append(encoding_control1_tokens_batch)
+            gt_data['encoding_control2_tokens'].append(encoding_control2_tokens_batch)
+            gt_data['decoding_control1_tokens'].append(decoding_control1_tokens_batch)
+            gt_data['decoding_control2_tokens'].append(decoding_control2_tokens_batch)
+            gt_data['decoding_control3_tokens'].append(decoding_control3_tokens_batch)
+
+            # Forward pass
+            hvo, latent_z = model.predict(
+                flat_hvo_groove=input_grooves_batch,
+                encoding_control_tokens=encoding_control_tokens,
+                decoding_control_tokens=decoding_control_tokens,
+            )
+
+            # Store predicted data
+            pred_data['output_streams'].append(hvo)
+
+    # convert to numpy    arrays
+    gt_data = {k: torch.cat(v, dim=0).numpy() for k, v in gt_data.items()}
+    pred_data['output_streams'] = torch.cat(pred_data['output_streams'], dim=0).numpy()
+    pred_data['flat_outputs'] = get_flat_from_streams(pred_data['output_streams'])
+
+    # Decoding control 1: Hamming distance between groove and flat output hits
+    ctrl1_untokenized = np.array(
+        [hamming_distance(a, b) for a, b in zip(gt_data['input_grooves'][:, :, 0], pred_data['flat_outputs'][:, :, 0])])
+    pred_data['encoding_control1_tokens'] = np.array(TokenizeControls(ctrl1_untokenized, n_bins=33, low=0, high=1))
+
+    # Decoding control 2: Hamming distance between groove and flat output accented hits
+    pred_accents = get_accent_hits_from_velocities(pred_data['flat_outputs'][:, :, 1], accent_thresh=0.75)
+    gt_accents = get_accent_hits_from_velocities(gt_data['input_grooves'][:, :, 1], accent_thresh=0.75)
+    ctr2_untokenized = np.array(
+        [hamming_distance(a, b, normalize=True) for a, b in zip(gt_accents, pred_accents)])
+    pred_data['encoding_control2_tokens'] = np.array(TokenizeControls(ctr2_untokenized, n_bins=10, low=0, high=0.85))
+
+    # Decoding controls 1-3: Hamming distance between flat output and each output stream
+    for i in range(3):
+        out_stream_i = pred_data['output_streams'][:, :, i]
+        ctrl_hammings_untokenized = np.array(
+            [hamming_distance(a, b, normalize=True) for a, b in zip(pred_data['flat_outputs'][:, :, 0], pred_data['output_streams'][:, :, i])])
+        pred_data[f'decoding_control{i + 1}_tokens'] = np.array(TokenizeControls(
+            ctrl_hammings_untokenized, n_bins=10, low=0, high=0.85
+        ))
+
+    return gt_data, pred_data, metadata
+
+def run_inference_flex_model(model, dataloader):
+    model.eval()
+    inputs_gt = []
+    outputs_gt = []
+    flat_outputs_gt = []
+    metadata = []
+    encoding_control1_tokens_gt = []
+    encoding_control2_tokens_gt = []
+    decoding_control1_tokens_gt = []
+    decoding_control2_tokens_gt = []
+    decoding_control3_tokens_gt = []
+    outputs_pred = []
+
+    with torch.no_grad():
+        for batch in tqdm.tqdm(dataloader):
+            input_grooves_batch = batch[0]
+            output_streams_gt_batch = batch[1]
+            encoding_control_tokens = batch[2]
+            decoding_control_tokens = batch[3]
+            encoding_control1_tokens_batch = encoding_control_tokens[:, 0]
+            encoding_control2_tokens_batch = encoding_control_tokens[:, 1]
+            decoding_control1_tokens_batch = decoding_control_tokens[:, 0]
+            decoding_control2_tokens_batch = decoding_control_tokens[:, 1]
+            decoding_control3_tokens_batch = decoding_control_tokens[:, 2]
+            indices_batch = batch[-1]
+            metadata.extend([dataloader.dataset.metadata[i] for i in indices_batch])
+
+            # Store ground truth data
+            inputs_gt.append(input_grooves_batch)
+            outputs_gt.append(output_streams_gt_batch)
+            encoding_control1_tokens_gt.append(encoding_control1_tokens_batch)
+            encoding_control2_tokens_gt.append(encoding_control2_tokens_batch)
+            decoding_control1_tokens_gt.append(decoding_control1_tokens_batch)
+            decoding_control2_tokens_gt.append(decoding_control2_tokens_batch)
+            decoding_control3_tokens_gt.append(decoding_control3_tokens_batch)
+
+            # Forward pass
+            hvo, latent_z = model.predict(
+                flat_hvo_groove=input_grooves_batch,
+                encoding_control_tokens=encoding_control_tokens,
+                decoding_control_tokens=decoding_control_tokens,
+            )
+            # Store predicted data
+            outputs_pred.append(hvo)
+
+    # Convert to numpy arrays
+    inputs_gt = torch.cat(inputs_gt, dim=0).cpu().numpy()
+    outputs_gt = torch.cat(outputs_gt, dim=0).cpu().numpy()
+    encoding_control1_tokens_gt = torch.cat(encoding_control1_tokens_gt, dim=0).cpu().numpy()
+    encoding_control2_tokens_gt = torch.cat(encoding_control2_tokens_gt, dim=0).cpu().numpy()
+    decoding_control1_tokens_gt = torch.cat(decoding_control1_tokens_gt, dim=0).cpu().numpy()
+    decoding_control2_tokens_gt = torch.cat(decoding_control2_tokens_gt, dim=0).cpu().numpy()
+    decoding_control3_tokens_gt = torch.cat(decoding_control3_tokens_gt, dim=0).cpu().numpy()
+    outputs_pred = torch.cat(outputs_pred, dim=0).cpu().numpy()
+    flat_outputs_gt = get_flat_from_streams(outputs_gt)
+    flat_outputs_pred = get_flat_from_streams(outputs_pred)
+
+    # Create a dictionary to hold all data
+    data_dict = {
+        'input_grooves_gt': inputs_gt,
+        'output_streams_gt': outputs_gt,
+        'flat_outputs_gt': flat_outputs_gt,
+        'encoding_control1_tokens_gt': encoding_control1_tokens_gt,
+        'encoding_control2_tokens_gt': encoding_control2_tokens_gt,
+        'decoding_control1_tokens_gt': decoding_control1_tokens_gt,
+        'decoding_control2_tokens_gt': decoding_control2_tokens_gt,
+        'decoding_control3_tokens_gt': decoding_control3_tokens_gt,
+        'output_streams_pred': outputs_pred,
+        'flat_outputs_pred': flat_outputs_pred,
+        'metadata': metadata
+    }
+
+    return data_dict
+
+def extract_control_features_flex_model(flat_input_hvo, flat_output_hvo, output_streams_hvo):
+    """
+    Extract control features from flat input HVO, flat output HVO, and output streams HVO.
+
+    Args:
+        flat_input_hvo: (B, T, 3) numpy array of flat input HVO
+        flat_output_hvo: (B, T, 3) numpy array of flat output HVO
+        output_streams_hvo: (B, T, 9) numpy array of output streams HVO
+
+    Returns:
+        Dictionary containing control features.
+    """
+    control_features = {}
+
+    # Encoding Control 1: Hamming distance between groove and flat output hits
+    ctrl1_untokenized = np.array(
+        [hamming_distance(a, b) for a, b in zip(flat_input_hvo[:, :, 0], flat_output_hvo[:, :, 0])])
+    control_features['encoding_control1_tokens'] = TokenizeControls(ctrl1_untokenized, n_bins=33, low=0, high=1)
+
+    # Encoding Control 2: Hamming distance between groove and flat output accented hits
+    gt_accents = get_accent_hits_from_velocities(flat_input_hvo[:, :, 1], accent_thresh=0.75)
+    pred_accents = get_accent_hits_from_velocities(flat_output_hvo[:, :, 1], accent_thresh=0.75)
+
+    gt_pred_hit_union = np.array(
+        [get_timesteps_where_either_is_1(a, b).shape[0] for a, b in zip(flat_input_hvo[:, :, 0], flat_output_hvo[:, :, 0])])
+    # Normalize by the number of steps where either has a hit
+    ctrl2_untokenized = np.array(
+        [hamming_distance(a, b) for a, b in zip(gt_accents, pred_accents)])
+
+    control_features['decoding_control1_tokens'] = []
+    for i, t in enumerate(gt_pred_hit_union):
+        if t > 0:
+            ctrl2_untokenized[i] = ctrl2_untokenized[i] / t * 32.0
+        else:
+            ctrl2_untokenized[i] = 0.0         # both silent
+
+    control_features['encoding_control2_tokens'] = TokenizeControls(ctrl2_untokenized, n_bins=5, low=0, high=0.85)
+
+    # Decoding Controls 1-3: Hamming distance between flat output and each output stream
+    for i in range(3):
+        out_stream_i = output_streams_hvo[:, :, i]
+        ctrl_hammings_untokenized = np.array(
+            [hamming_distance(a, b, normalize=True) for a, b in zip(flat_output_hvo[:, :, 0], out_stream_i)])
+        control_features[f'decoding_control{i + 1}_tokens'] = TokenizeControls(
+            ctrl_hammings_untokenized, n_bins=6, low=0, high=0.85
+        )
+
+    return control_features
